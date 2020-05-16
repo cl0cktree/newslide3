49087b84d905616d941cb03a43c18b0052f51fc2
Lee.Hyunsup <clocktree@live.co.kr>
2020-05-16 20:14:26 +0900

    20200516_2
    - 수정.

--- js/move.js
+++ js/move.js
@@ -171,7 +171,7 @@ $(function(){
 					startbar();
 					// inner_controll_s();
 				}
-				else if (event.type='click')
+				else if (event.type=='click')
 				{
 					// event.preventDefault();
 					// event.stopPropagation();
